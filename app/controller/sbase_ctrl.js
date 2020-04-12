27c27cf806df5207e978089a4c433ff7df1631f4
Do Minh Phong <phongdo@beeketing.net>
2020-04-12 23:24:56 +0700

    [WIP] Add sbase tool

--- app/controller/sbase_ctrl.js
+++ app/controller/sbase_ctrl.js
@@ -1,4 +1,4 @@
-app.controller('sbaseCtrl', function () {
+app.controller('sbaseCtrl', function (mainFactory) {
     var vm = this;
     const processStep = [
         'validate_url',
@@ -20,7 +20,6 @@ app.controller('sbaseCtrl', function () {
     const process = [];
 
     vm.sbaseData = {
-        shop: {},
         shopUrl: {},
         boostrap: {},
         env: 'Production',
@@ -30,55 +29,127 @@ app.controller('sbaseCtrl', function () {
     vm.productSingle = {
         id: 0,
         url: '',
+        handle: '',
         cacheKeySingle: 'single',
-        caceKeyMap: 'map',
+        cacheKeyMap: 'map',
         isWrongCache: false,
         data: {},
         error: '',
-        currentStep: 'Chưa làm gì'
+        debugLogs: ['Chưa làm gì'],
     }
 
-    function debugProductSingle() {
-        let isValidUrl = validateUrl(SBASE_TYPE.product_single, vm.productSingle.url);
-        if (!isValidUrl) {
+    // functions
+    vm.debugProductSingle = debugProductSingle
+
+    function setStep(step) {
+        vm.productSingle.debugLogs.push(step);
+    }
+
+    async function detectEnv(url) {
+        url = url.replace('http://', '')
+        url = url.replace('https://', '')
+        let returnObj = {
+            env: 'dev',
+            envErr: '',
+        }
+
+        // if(url.includes('.onshopbase.com')){
+        //     returnObj.env = 'prod';
+        //     return returnObj
+        // }
+        //
+        // if(url.includes('stag.myshopbase.net')){
+        //     returnObj.env = 'stag';
+        //     return returnObj
+        // }
+        //
+        // if(url.includes('.myshopbase.net')){
+        //     returnObj.env = 'dev';
+        //     return returnObj
+        // }
+
+        // Try to get bootstrap
+        let bootstrap = await mainFactory.doGetAnyway('', `https://api.shopbase.com/api/bootstrap/${url}.json`)
+        console.log('Data got', bootstrap)
+        if (!bootstrap.error) {
+            returnObj.env = 'prod';
+            vm.sbaseData.boostrap = bootstrap
+            return returnObj
+        }
+
+        bootstrap = await mainFactory.doGetAnyway('', `https://gapi.stag.shopbase.net/api/bootstrap/${url}.json`)
+        console.log('Data got', bootstrap)
+        if (!bootstrap.error) {
+            returnObj.env = 'stag';
+            vm.sbaseData.boostrap = bootstrap
+            return returnObj
+        }
+
+        bootstrap = await mainFactory.doGetAnyway('', `https://gapi.dev.shopbase.net/api/bootstrap/${url}.json`)
+        console.log('Data got', bootstrap)
+        if (!bootstrap.error) {
+            returnObj.env = 'dev';
+            vm.sbaseData.boostrap = bootstrap
+            return returnObj
+        }
+
+        returnObj.envErr = 'Cannot detect env';
+        return returnObj;
+    }
+
+    function buildCacheKeys() {
+        //10065965:storefront:products-by-handle:the-cats
+        // 10065965:storefront:product-id-to-handle:1000000056249644
+    }
+
+    async function debugProductSingle() {
+        vm.productSingle.debugLogs = [];
+        setStep('Bắt đầu debug')
+        setStep('Validate url')
+        let {handle, shopUrl, isValid} = validateUrl(SBASE_TYPE.product_single, vm.productSingle.url);
+        if (!isValid) {
+            setStep('Validate url error')
             vm.productSingle.error = 'Invalid url';
             return
         }
+        vm.sbaseData.shopUrl = shopUrl
+        vm.productSingle.handle = handle
 
-        let {shop, shopErr} = getShop();
-        if (shopErr) {
-            vm.productSingle.error = shopErr;
-            return
-        }
-        vm.sbaseData.shop = shop
-
-        let {env, envErr} = detectEnv();
+        let {env, envErr} = await detectEnv(shopUrl);
         if (envErr) {
             vm.productSingle.error = envErr;
             return;
         }
+        console.log(`Set env: ${env}`)
+        console.log(`Set env: `, vm.sbaseData.boostrap)
+        vm.sbaseData.environ = shopUrl;
 
-        vm.sbaseData.env = env;
+        // build cachekeys
+        // let {cache1, cache2} = buildCacheKeys();
 
-        let {bootstrap, bootstrapErr} = getBootstrap(vm.productSingle.url, env);
-        if (bootstrapErr) {
-            vm.productSingle.error = bootstrapErr;
-            return;
-        }
-
-        let objCache = {};
-        let objNotCache = {};
-        vm.productSingle.isWrongCache = objCache === objNotCache;
-        vm.cacheKeys = buildCacheKeys(SBASE_TYPE.product_single);
+        // let objCache = {};
+        // let objNotCache = {};
+        // vm.productSingle.isWrongCache = objCache === objNotCache;
+        // vm.cacheKeys = buildCacheKeys(SBASE_TYPE.product_single);
     }
 
     function validateUrl(type, url) {
-        switch (type) {
-            case SBASE_TYPE.product_single:
-                break;
+        let returnObj = {
+            handle: '',
+            shopUrl: '',
+            isValid: true,
         }
+        var paths = url.split('/products/');
+        if (!paths || paths.length !== 2) {
+            returnObj.isValid = false;
+            return returnObj
+        }
+        returnObj.handle = paths[1];
 
-        return true
+        paths = url.split('/');
+        returnObj.shopUrl = paths[0] + '//' + paths[2];
+
+        return returnObj
     }
 
 });
--- app/module/main.factory.js
+++ app/module/main.factory.js
@@ -1,32 +1,46 @@
 angular
-        .module('app')
-        .factory('mainFactory', function ($http) {
-            var factory = {
-                doPost: doPost,
-                doGet: doGet
-            };
+    .module('app')
+    .factory('mainFactory', function ($http) {
+        var factory = {
+            doPost: doPost,
+            doGet: doGet,
+            doGetAnyway: doGetAnyway,
+        };
 
-            function doPost(data, uri) {
-                return  $http({
-                    url: uri,
-                    method: "POST",
-                    headers: {'Content-Type': 'application/json'},
-                    data: data
-                }).then(function (response) {
-                    return response.data;
-                });
-            }
+        function doPost(data, uri) {
+            return $http({
+                url: uri,
+                method: "POST",
+                headers: {'Content-Type': 'application/json'},
+                data: data
+            }).then(function (response) {
+                return response.data;
+            });
+        }
 
-            function doGet(data, uri) {
-                return  $http({
-                    url: uri,
-                    method: "GET",
-                    headers: {'Content-Type': 'application/json'},
-                    data: data
-                }).then(function (response) {
-                    debugger;
-                    return response.data;
-                });
-            }
-            return factory;
-        });
+        function doGet(data, uri) {
+            return $http({
+                url: uri,
+                method: "GET",
+                headers: {'Content-Type': 'application/json'},
+                data: data
+            }).then(function (response) {
+                return response.data;
+            });
+        }
+
+        function doGetAnyway(data, uri) {
+            return $http({
+                url: uri,
+                method: "GET",
+                headers: {'Content-Type': 'application/json'},
+                data: data
+            }).then(function (res) {
+                return res.data
+            }).catch(function (err) {
+                return err.data
+            });
+        }
+
+        return factory;
+    });
